d96536d1643672193a0016f1c8d1ecf08852b0fe
Maham Shahid <161121868+MAHAM42@users.noreply.github.com>
2024-04-25 07:51:26 -0700

    19-Assignment
    TypeScript 45-Assignment of Q19 with Node.js

---
+++ main19.js
@@ -0,0 +1,11 @@
+"use strict";
+// ASSIGNMENT NO 19:
+/*
+Dinner Guests: Working with one of the programs from Exercises 14 through 18,
+ print a message indicating the number of people you are inviting to dinner.
+ */
+console.log("ASSIGNMENT NO 19:");
+let guestArr = ["Amna", "Hadia", "Asma"];
+//guestArr.map((items) => (console.log(`Dear ${items} , You are invited to the dinner.`)));
+let lengthGuestArr = guestArr.length;
+console.log(`We are Inviting total ${lengthGuestArr} guests.`);
---
+++ main19.ts
@@ -0,0 +1,16 @@
+// ASSIGNMENT NO 19:
+
+/*
+Dinner Guests: Working with one of the programs from Exercises 14 through 18,
+ print a message indicating the number of people you are inviting to dinner.
+ */
+
+ console.log("ASSIGNMENT NO 19:");
+
+ let guestArr : string [] = ["Amna", "Hadia", "Asma"];
+
+ //guestArr.map((items) => (console.log(`Dear ${items} , You are invited to the dinner.`)));
+
+ let lengthGuestArr: number = guestArr.length;
+
+ console.log(`We are Inviting total ${lengthGuestArr} guests.`);
